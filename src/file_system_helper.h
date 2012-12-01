13a5d972b8d974cf3b2c853998db44c11504fae9
google.techianprakash <google.techianprakash@gmail.com>
2012-11-30 18:09:17 -0800

    added file helpers

---
+++ src/file_system_helper.h
@@ -0,0 +1,13 @@
+/*
+File System helper header
+*/
+
+#ifndef FILE_SYSTEM_HELPER_H
+#define FILE_SYSTEM_HELPER_H
+
+class FileSystemHelper {
+ public:
+  void Connect();
+};
+
+#endif /* FILE_SYSTEM_HELPER_H */
