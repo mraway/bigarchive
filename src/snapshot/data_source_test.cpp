5c70528200d8ea74fe7b8898c39d12cdd5094325
Wei Zhang <mraway@gmail.com>
2013-01-25 17:50:45 -0800

    Update data source test

--- src/snapshot/data_source_test.cpp
+++ src/snapshot/data_source_test.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    string trace("");
-    string sample_data("");
+    string trace("/mnt/data2/scanlog/win2003_64/data/VM-2C0A05C1.1000-19189-8071-merged.vhd.bv4");
+    string sample_data("sample_data");
     string output_file("data_source_test.output");
     ofstream os(output_file.c_str(), ios_base::out | ios_base::binary | ios_base::trunc);
     DataSource source(trace, sample_data);
     SegmentMeta sm;
     while (source.GetSegment(sm)) {
-        for (int i = 0; i < sm.block_list_.size(); ++i)
+        for (size_t i = 0; i < sm.block_list_.size(); ++i)
         {
             os.write(sm.block_list_[i].data_, sm.GetBlockSize(i));
         }
