7fd08f0a8b8f46f66600f1eaead89aa2db9928fd
Wei Zhang <mraway@gmail.com>
2013-01-28 05:45:06 -0800

    update some source files for build system

--- src/test/append_store_read.cpp
+++ src/test/append_store_read.cpp
@@ -1,26 +1,26 @@
 #include "store.h"
-#include "append_store.h"
+#include "../append-store/append_store.h"
 #include <iostream>
-#include "append_store_types.h"
+#include "../append-store/append_store_types.h"
 // #include 
 
 using namespace std;
 
 int main() {
 
- cout << endl << "main entry point";
+    cout << endl << "main entry point";
  
- StoreParameter sp = StoreParameter(); 
- sp.mPath = "//TEST//SIZE_64//TRY_25";
- sp.mAppend = false; 
- PanguAppendStore *pas = new PanguAppendStore(sp, false);
- string data_read;
- for(int i=1; i <= 16384; i++) {
-  Handle h;
-  h.mChunkId = 0;
-  h.mIndex = i;
-  pas->Read(h.ToString(), &data_read);
-  cout << endl << "data_read : " << data_read.substr(0,50);
- }
- pas->Close();
+    StoreParameter sp = StoreParameter(); 
+    sp.mPath = "//TEST//SIZE_64//TRY_25";
+    sp.mAppend = false; 
+    PanguAppendStore *pas = new PanguAppendStore(sp, false);
+    string data_read;
+    for(int i=1; i <= 16384; i++) {
+        Handle h;
+        h.mChunkId = 0;
+        h.mIndex = i;
+        pas->Read(h.ToString(), &data_read);
+        cout << endl << "data_read : " << data_read.substr(0,50);
+    }
+    pas->Close();
 }
