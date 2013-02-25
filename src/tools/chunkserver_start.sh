40b94a4bdb083420fa5ec4cbf952c6f899a2d267
Wei Zhang <mraway@gmail.com>
2013-02-25 13:44:46 -0800

    chunkserver script

--- src/tools/chunkserver_start.sh
+++ src/tools/chunkserver_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../qfs/build/release/bin/chunkserver ../config/Chunkserver.prp ./chunkserver.log > ./chunkserver.out 2>&1 &
+../qfs/build/release/bin/chunkserver ../config/ChunkServer.prp ./chunkserver.log > ./chunkserver.out 2>&1 &
