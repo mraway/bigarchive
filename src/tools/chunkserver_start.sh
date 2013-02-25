83567581bef8cec973796c76eaad47fc5ab6fa4b
Wei Zhang <mraway@gmail.com>
2013-02-25 13:42:41 -0800

    qfs init scripts

---
+++ src/tools/chunkserver_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../qfs/build/release/bin/chunkserver ../config/Chunkserver.prp ./chunkserver.log > ./chunkserver.out 2>&1 &
---
+++ src/tools/metaserver_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../qfs/build/release/bin/metaserver -c ../config/MetaServer.prp ./metaserver.log > ./metaserver.out 2>&1 &
