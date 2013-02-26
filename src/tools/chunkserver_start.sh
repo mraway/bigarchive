8c01518c15d7566c7f4d6c52d44c9532c1b6e79b
Wei Zhang <mraway@gmail.com>
2013-02-25 18:15:12 -0800

    Update qfs location

--- src/tools/chunkserver_start.sh
+++ src/tools/chunkserver_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../qfs/build/release/bin/chunkserver ../config/ChunkServer.prp ./chunkserver.log > ./chunkserver.out 2>&1 &
+/home/qfs/build/release/bin/chunkserver /home/bigarchive/config/ChunkServer.prp ~/chunkserver.log > ~/chunkserver.out 2>&1 &
--- src/tools/metaserver_start.sh
+++ src/tools/metaserver_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../qfs/build/release/bin/metaserver -c ../config/MetaServer.prp ./metaserver.log > ./metaserver.out 2>&1 &
+/home/qfs/build/release/bin/metaserver -c /home/bigarchive/config/MetaServer.prp ~/metaserver.log > ~/metaserver.out 2>&1 &
