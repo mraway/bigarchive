107dc15ba5999bb3f0616ca35423ddbf1c3fc034
Wei Zhang <mraway@gmail.com>
2013-02-26 00:06:57 -0800

    qfs reset script

---
+++ src/tools/qfs_reset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf /mnt/data1/qfs/*
+rm -rf ~/chunkserver.log
+rm -rf ~/chunkserver.out
+rm -rf ~/metaserver.log
+rm -rf ~/metaserver.out
+rm -rf /home/bgtest/qfs_log/*
+rm -rf /home/bgtest/qfs_checkpoint/*
