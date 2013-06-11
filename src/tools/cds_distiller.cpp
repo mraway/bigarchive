5941af46672f15aa6bc486d68df4434b94ec829d
Wei Zhang <mraway@gmail.com>
2013-06-11 13:44:31 -0700

    CDS generator

--- src/tools/cds_distiller.cpp
+++ src/tools/cds_distiller.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <map>
 #include <algorithm>
+#include <math.h>
 #include "../snapshot/trace_types.h"
 
 using namespace std;
@@ -16,6 +17,13 @@ struct CdsCount {
     CdsCount() {total_size = 0; num_blocks = 0;}
 };
 
+uint64_t raw_blocks = 0;
+uint64_t raw_size = 0;
+uint64_t total_blocks = 0;
+uint64_t total_size = 0;
+uint64_t dedup_size = 0;
+uint64_t dedup_blocks = 0;
+
 void usage(char* progname)
 {
     cout << "Usage: " << progname << " cds_name vm_list [prefix] [suffix]" << endl;
@@ -25,7 +33,7 @@ void usage(char* progname)
     cout << "suffix is used to specify the trace type, such like .bv4 or .v4" << endl;
 }
 
-void analysis_cds(map<Block, uint32_t>& global_index, uint64_t total_size)
+void analysis_cds(map<Block, uint32_t>& global_index, const string& cds_name, bool write_out = false)
 {
     // aggregate by count
     map<Block, uint32_t>::iterator it;
@@ -37,8 +45,8 @@ void analysis_cds(map<Block, uint32_t>& global_index, uint64_t total_size)
     }
 
     // calculate the dedup size
-    uint64_t dedup_size = 0;
-    uint64_t dedup_blocks = 0;
+    dedup_size = 0;
+    dedup_blocks = 0;
     for (rit = cds_counter.rbegin(); rit != cds_counter.rend(); rit++) {
         dedup_blocks += rit->second.num_blocks;
         dedup_size += rit->second.total_size;
@@ -46,33 +54,67 @@ void analysis_cds(map<Block, uint32_t>& global_index, uint64_t total_size)
     }
 
     // calculate cds coverage from 1% to 10%
-    uint64_t cds_size_threshold[10];
-    uint64_t cds_freq_threshold[10];
+    uint64_t cds_size_threshold[10];	// this is how many FPs CDS shall hold
     for (int i = 0; i < 10; i++) {
-        cds_size_threshold[i] = (i + 1) * dedup_size / 100;
+        cds_size_threshold[i] = (i + 1) * dedup_blocks / 100;
         //cout << cds_size_threshold[i] << endl;
     }
 
-    // get the freq threshold 
-    uint64_t cds_size = 0;
-    uint64_t cds_coverage = 0;
-    int i = 0;
-    for (rit = cds_counter.rbegin(); rit != cds_counter.rend(); rit++) {
-        cds_size += rit->second.total_size;
-        cds_coverage += rit->second.total_size * rit->first;
-        while (cds_size > cds_size_threshold[i]) {
-            cds_freq_threshold[i] = rit->first;
-            cout << "  threshold: " << (i + 1) << "% " << "freq: " << cds_freq_threshold[i]
-                 << " CDS size: " << (double)100 * cds_size / dedup_size << "% "
-                 << cds_size 
-                 << " covered raw data: " << (double)100 * cds_coverage / total_size << "% " 
-                 << cds_coverage << endl;
-            if (++i >= 10) {
+    uint64_t cds_freq_threshold[10];		// this is the frequency threshold
+    uint64_t num_to_add[10];
+    for (int i = 0; i < 10; i++) {
+        uint64_t cds_size = 0;
+        uint64_t cds_data_coverage = 0;
+        for (rit = cds_counter.rbegin(); rit != cds_counter.rend(); rit++) {
+            if ((cds_size + rit->second.num_blocks) >= cds_size_threshold[i]) {
+                num_to_add[i] = cds_size_threshold[i] - cds_size;
+                cds_data_coverage += rit->first * num_to_add[i] * (rit->second.total_size / rit->second.num_blocks);
+                cds_size = cds_size_threshold[i];
+                cds_freq_threshold[i] = rit->first;
                 break;
             }
+            else {
+                cds_size += rit->second.num_blocks;
+                cds_data_coverage += rit->first * rit->second.total_size;
+            }
         }
-        if (i >= 10) {
-            break;
+        if (!write_out) {
+            cout << "  threshold: " << (i + 1) << "% " << "freq: " << cds_freq_threshold[i]
+                 << std::fixed << std::setw(5) << std::setprecision(6) << std::setfill( '0' )
+                 << " CDS index size: " << (double)100 * cds_size / dedup_blocks << "% " << cds_size 
+                 << " covered raw data: " << (double)100 * cds_data_coverage / total_size << "% " << cds_data_coverage 
+                 << " alpha: " << (1 - log((double)cds_data_coverage / total_size) / log((double)cds_size / dedup_blocks))
+                 << endl;
+        }
+    }
+
+    // scan the global index to generate CDS index according to threshold
+    // we will only generate CDS from 1% to 5%
+    if (write_out) {
+        ofstream cds_output[5];
+        for (int i = 0; i < 5; i++) {
+            stringstream ss;
+            ss << cds_name << "." << setfill('0') << setw(3) << (i+1);
+            cds_output[i].open(ss.str().c_str(), ios::out | ios::binary | ios::trunc);
+        }
+
+        for (it = global_index.begin(); it != global_index.end(); ++ it) {
+            for (int i = 0; i < 5; ++i) {
+                if (it->second > cds_freq_threshold[i]) {
+                    it->first.ToStream(cds_output[i]);
+                    continue;
+                }
+                if (it->second == cds_freq_threshold[i]) {
+                    if (num_to_add[i] > 0) {
+                        it->first.ToStream(cds_output[i]);
+                        --num_to_add[i];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; ++i) {
+            cds_output[i].close();
         }
     }
 }
@@ -93,12 +135,9 @@ int main(int argc, char** argv)
     if (argc > 4) {
         fsuffix = argv[4];
     }
+    bool localized_dedup = true;
 
     // build the global index with counter
-    uint64_t raw_blocks = 0;
-    uint64_t raw_size = 0;
-    uint64_t total_blocks = 0;
-    uint64_t total_size = 0;
     map<Block, uint32_t> global_index;
     map<Block, uint32_t>::iterator it;
     string vm_fname;
@@ -136,13 +175,13 @@ int main(int argc, char** argv)
                 raw_blocks += segs[j].blocklist_.size();
                 raw_size += segs[j].size_;
                 // level 1
-                if (j > 0 && segs[j] == segs[j-1]) {
+                if (j > 0 && localized_dedup && segs[j] == segs[j-1]) {
                     continue;
                 }
                 // level 2
                 for (vector<Block>::iterator it = segs[j].blocklist_.begin(); 
                      it != segs[j].blocklist_.end(); ++it) {
-                    if (j == 0) {
+                    if (j == 0 || !localized_dedup) {
                         global_index[*it] += 1;
                         total_size += it->size_;
                         total_blocks += 1;
@@ -169,10 +208,23 @@ int main(int argc, char** argv)
             trace_inputs[j]->close();
             delete trace_inputs[j];
         }
-        analysis_cds(global_index, total_size);
+        analysis_cds(global_index, cds_name);
     }
     vm_ifs.close();
 
+    cout << "raw:" << endl;
+    cout << "blocks: " << raw_blocks << endl;
+    cout << "size: " << raw_size / float(1024*1024*1024) << " GB" << endl;
+    cout << "after localized dedup: " << endl;
+    cout << "blocks: " << total_blocks << endl;
+    cout << "size: " << total_size / float(1024*1024*1024) << " GB" << endl;
+    cout << "after complete dedup:" << endl;
+    cout << "blocks: " << dedup_blocks << endl;
+    cout << "size: " << dedup_size / float(1024*1024*1024) << " GB" << endl;
+
+    analysis_cds(global_index, cds_name, true);
+
+    /*
     // aggregate by count
     map<uint32_t, CdsCount> cds_counter;
     map<uint32_t, CdsCount>::reverse_iterator rit;
@@ -189,15 +241,6 @@ int main(int argc, char** argv)
         dedup_size += rit->second.total_size;
         //cout << rit->first << " " << rit->second.num_blocks << " " << rit->second.total_size << endl;
     }
-    cout << "raw:" << endl;
-    cout << "blocks: " << raw_blocks << endl;
-    cout << "size: " << raw_size / float(1024*1024*1024) << " GB" << endl;
-    cout << "after localized dedup: " << endl;
-    cout << "blocks: " << total_blocks << endl;
-    cout << "size: " << total_size / float(1024*1024*1024) << " GB" << endl;
-    cout << "after complete dedup:" << endl;
-    cout << "blocks: " << dedup_blocks << endl;
-    cout << "size: " << dedup_size / float(1024*1024*1024) << " GB" << endl;
 
     // calculate cds coverage from 1% to 10%
     uint64_t cds_size_threshold[10];
@@ -248,7 +291,7 @@ int main(int argc, char** argv)
     for (i = 0; i < 5; ++i) {
         cds_output[i].close();
     }
-
+    */
     return 0;
 }
 
