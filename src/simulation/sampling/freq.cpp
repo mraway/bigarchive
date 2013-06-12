8f1eb2eeae9a07f49a95098306ecc47601e5ec1f
Michael Agun <mikeagun@gmail.com>
2013-06-11 23:36:06 -0700

    finished block frequency counting program. still need to support pre-computed cds

--- src/simulation/sampling/freq.cpp
+++ src/simulation/sampling/freq.cpp
@@ -139,7 +139,64 @@ void theoretical(std::vector<Segment>& indexBlocks, ifstream& trace, double blk_
     printf("theoretical dedup ratio = %d/%d (%3.3f%%)\n",hits,blocks,(double)(100*hits)/(double)blocks);
 }
 
-void do_block_freq(map<Hash, int>& block_freq, ifstream& trace, double blk_threshold, unsigned int seed) {
+void min_heapify(int *heap, int len, int i)
+{
+    int left,right,smallest,t;
+    do {
+        left = (i << 1) + 1;
+        right = (i << 1) + 2;
+        smallest = i;
+        if (left < len && heap[left] < heap[smallest])
+            smallest = left;
+        if (right < len && heap[right] < heap[smallest])
+            smallest = right;
+        if (smallest != i) {
+            t = heap[i];
+            heap[i] = heap[smallest];
+            heap[smallest] = t;
+            i = smallest;
+        } else {
+            break;
+        }
+    } while (i < ((len - 1) & (~1)));
+}
+
+void delete_min(int* heap, int *len)
+{
+    if (*len < 1)
+        return;
+    (*len)--;
+    if (*len > 0)
+    {
+        heap[0] = heap[*len];
+        min_heapify(heap,*len,0);
+    }
+}
+
+void insert_min(int *heap, int *len, int n)
+{
+    int i = *len;
+    int parent = (i - 1) >> 1;
+    int t;
+    heap[i] = n;
+    while(i > 0 && heap[parent] > heap[i]) {
+        t = heap[i];
+        heap[i] = heap[parent];
+        heap[parent] = t;
+        i = parent;
+        parent = (i - 1) >> 1;
+    }
+    (*len)++;
+}
+
+void topk_heap(int* heap, int* cur_length, int max_length, int n)
+{
+    if (*cur_length >= max_length)
+        delete_min(heap,cur_length);
+    insert_min(heap,cur_length, n);
+}
+
+void do_block_freq(map<Hash, int>& block_freq, ifstream& trace, double cds_percent, double blk_threshold, unsigned int seed) {
     //trace.clear();
     //trace.seekg(0,std::ios::beg);
     if (blk_threshold > 0)
@@ -149,8 +206,9 @@ void do_block_freq(map<Hash, int>& block_freq, ifstream& trace, double blk_thres
     Hash current_hash;
     set<Hash> seg_blocks;
     int i;
-    int blocks = 0;
+    long int blocks = 0;
     int newBlocks = 0;
+    int uniqBlocks = 0;
     //printf("segments: %d\nblocks: %d\n",segments,blocks);
     while(load_rand_segment(current_seg, trace, blk_threshold)) {
         for (i = 0; i < current_seg.blocklist_.size(); i++) {
@@ -158,7 +216,9 @@ void do_block_freq(map<Hash, int>& block_freq, ifstream& trace, double blk_thres
             if (seg_blocks.find(current_hash) == seg_blocks.end()) {
                 seg_blocks.insert(current_hash);
                 block_freq[current_hash]++;
-                newBlocks++;
+                if (block_freq[current_hash] == 1)
+                    newBlocks++;
+                uniqBlocks++;
             }
             blocks++;
         }
@@ -166,26 +226,51 @@ void do_block_freq(map<Hash, int>& block_freq, ifstream& trace, double blk_thres
     }
 
     //pr_msg("trace block frequency scan finished");
-    printf("blocks read: %d\nunique blocks read: %d\n",blocks, newBlocks);
-    int max = 0;
-    int maxCount = 0;
+    printf("new index size: %d\n", block_freq.size());
+    printf("blocks read: %lld\nunique blocks read: %d\nnew blocks read: %d\n",blocks, uniqBlocks,newBlocks);
     int sum = 0;
+    int max = 0;
     int entries = 0;
-    for(iter = block_freq.begin(); iter != block_freq.end(); ++iter)
-    {
-        int count = iter->second;
-        if (count > max) {
-            max = count;
-            maxCount = 0;
-        } else if (count == max) {
-            maxCount++;
+    if (cds_percent > 0) {
+        int cds_size = (int)((double)(block_freq.size()) * cds_percent);
+        int cds_len = 0;
+        int cds_sum = 0;
+        int *cds_entries = (int*)malloc(sizeof(int)*cds_size);
+
+        for(iter = block_freq.begin(); iter != block_freq.end(); ++iter) {
+            int count = iter->second;
+            topk_heap(cds_entries, &cds_len, cds_size, count);
+            sum += count;
+            entries++;
+            //printf("%d\n",count);
         }
-        sum += count;
-        entries++;
-        //printf("%d\n",count);
+        for(i = 0; i < cds_len; i++) {
+            cds_sum += cds_entries[i];
+            if (cds_entries[i] > max) {
+                max = cds_entries[i];
+            }
+        }
+        printf("Max Links: %d\nCDS Entries: %d\n",max,cds_len);
+        free(cds_entries);
+        printf("Avg links (including CDS blocks): %g\n", (double)sum / (double)entries);
+        printf("Avg links (excluding CDS blocks): %g\n", (double)(sum-cds_sum) / (double)(entries - cds_len));
+        printf("Avg CDS block links: %g\n", (double)cds_sum / (double)cds_len);
+    } else {
+        int maxCount = 0;
+        for(iter = block_freq.begin(); iter != block_freq.end(); ++iter) {
+            int count = iter->second;
+            if (count > max) {
+                max = count;
+                maxCount = 1;
+            } else if (count == max) {
+                maxCount++;
+            }
+            sum += count;
+            entries++;
+            //printf("%d\n",count);
+        }
+        printf("Max: %d\nMax Entries: %d\nAvg: %g\n", max, maxCount, (double)sum / (double)entries);
     }
-    printf("Max: %d\nMax Entries: %d\nAvg: %g\n", max, maxCount, (double)sum / (double)entries);
-    printf("index size: %d\n", block_freq.size());
 }
 
 void container_freq(map<Hash, int>& container_freq, map<int, int>& containerCounts, int& current_container, int& container_length, ifstream& trace, double blk_threshold, unsigned int seed) {
@@ -334,7 +419,7 @@ int main(int argc, char** argv)
     //std::vector<Segment> indexBlocks;
     //std::vector<Block>::iterator it;
     //Segment current_seg;
-    ifstream current_input, index_input, cds_input;
+    ifstream current_input, trace_input;
     //ofstream output;
     uint32_t i, j, k;
     //bool isdup;
@@ -353,7 +438,11 @@ int main(int argc, char** argv)
     bool do_dirty = false;
     bool do_parent = false;
     bool do_rand = false;
+    bool use_trace_file = false;
     double blk_threshold = 0;
+    double cds_percent = 0;
+    int cds_every = 1;
+    int trace_index = 0;
     unsigned int seed;
 
     while(argindex < argc && argv[argindex][0] == '-')
@@ -380,30 +469,86 @@ int main(int argc, char** argv)
             blk_threshold = atof(argv[argindex++]);
             seed = (unsigned int)atol(argv[argindex++]);
         }
-    }
-
-    if (argindex >= argc)
-    {
-        usage(argv[0]);
-        return 0;
-    }
-
-    while(argindex < argc) {
-        char *cur_trace_path = argv[argindex++];
-
-        current_input.open(cur_trace_path, std::ios_base::in | std::ios_base::binary);
-        if (!current_input.is_open()) {
-            pr_msg("unable to open %s", cur_trace_path);
-            exit(1);
+        else if (strcmp(argv[argindex],"--cds") == 0)
+        {
+            argindex++;
+            if (argindex >= argc)
+            {
+                usage(argv[0]);
+                return 0;
+            }
+            cds_percent = atof(argv[argindex++]);
         }
-        printf("file: %s\n",cur_trace_path);
-        //theoretical(indexBlocks, current_input, blk_threshold, seed);
-        //sampled(indexBlocks, current_input, cache_size, blk_threshold, seed, do_dirty, do_parent);
-        do_block_freq(block_freq, current_input, blk_threshold, seed);
+        else if (strcmp(argv[argindex],"--cdsevery") == 0)
+        {
+            argindex++;
+            if (argindex >= argc)
+            {
+                usage(argv[0]);
+                return 0;
+            }
+            cds_every = atoi(argv[argindex++]);
+        }
+	else if (strcmp(argv[argindex],"--tracefile") == 0)
+	{
+            use_trace_file = true;
+            argindex++;
+            if (argindex >= argc)
+            {
+                usage(argv[0]);
+                return 0;
+            }
+            trace_input.open(argv[argindex++], std::ios_base::in);
+            if (!trace_input.is_open()) {
+                pr_msg("unable to open %s", argv[argindex-1]);
+                exit(1);
+            }
+	}
+        else if (strcmp(argv[argindex],"--") == 0)
+        {
+            argindex++;
+            break;
+        }
+        else
+        {
+            cout << "Invalid argument. Use -- before using filename args beginning with '-'" << endl;
+            usage(argv[0]);
+            return 0;
+        }
+    }
 
-        //output.close();
-        //parent_input.close();
-        current_input.close();
+    while((!use_trace_file && argindex < argc) || (use_trace_file && trace_input.good())) {
+        string line;
+        const char *cur_trace_path;
+        double temp_cds_percent;
+        if (use_trace_file) {
+            getline(trace_input,line);
+            cur_trace_path = line.c_str();
+        } else {
+            cur_trace_path = argv[argindex++];
+        }
+
+        if (strlen(cur_trace_path) > 0 && cur_trace_path[0] != '\n') {
+            current_input.open(cur_trace_path, std::ios_base::in | std::ios_base::binary);
+            if (!current_input.is_open()) {
+                pr_msg("unable to open %s", cur_trace_path);
+                exit(1);
+            }
+            printf("file: %s\n",cur_trace_path);
+            //theoretical(indexBlocks, current_input, blk_threshold, seed);
+            //sampled(indexBlocks, current_input, cache_size, blk_threshold, seed, do_dirty, do_parent);
+            if ((trace_index % cds_every) == 0) {
+                temp_cds_percent = cds_percent;
+            } else {
+                temp_cds_percent = 0;
+            }
+            do_block_freq(block_freq, current_input, temp_cds_percent, blk_threshold, seed);
+
+            //output.close();
+            //parent_input.close();
+            current_input.close();
+        }
+        trace_index++;
     }
     return 0;
 }
